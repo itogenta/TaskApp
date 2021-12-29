219ef79f712dac0e33854f304a86a1b0732d38c4
Itou Genta <b1545276@planet.kanazawa-it.ac.jp>
2021-12-29 11:15:08 +0900

    Initial Commit

---
+++ TaskApp/TaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskAppApp.swift
+//  TaskApp
+//
+//  Created by 伊藤源太 on 2021/12/29.
+//
+
+import SwiftUI
+
+@main
+struct TaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
